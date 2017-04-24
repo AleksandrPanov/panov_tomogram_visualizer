ed9af54f8431555c4ab63abb975957da54580310
Alexander <Sasha1_1997@mail.ru>
2017-04-24 23:56:37 +0400

    add view

---
+++ panov_tomogram_visualizer/show/View.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace panov_tomogram_visualizer.show
+{
+    class View
+    {
+        void SetupView(int width, int height)
+        {
+            /*
+            GL.ShadeModel(ShadingModel.Smooth);
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadIdentity();
+            GL.Ortho(0, Bin.X, 0, Bin.Y, -1, 1);
+            GL.Viewport(0,0,width, height); 
+            */
+        }
+    }
+}
