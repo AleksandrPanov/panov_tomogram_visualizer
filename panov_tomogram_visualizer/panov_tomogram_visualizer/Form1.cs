a5d00d0ad2eb9ffed4514b19ed0700c4b2dad6e4
Alexandr PAnov <Sasha1_1997@mail.ru>
2017-04-28 11:51:43 +0300

    add count of FPS

--- panov_tomogram_visualizer/panov_tomogram_visualizer/Form1.cs
+++ panov_tomogram_visualizer/panov_tomogram_visualizer/Form1.cs
@@ -14,11 +14,75 @@ namespace panov_tomogram_visualizer
 {
     public partial class Form1 : Form
     {
-        Bin b = new Bin();
+        Bin bin;
+        View view;
+        bool isLoaded = false;
+        int curLayer = 0;
+        int FrameCount;
+        DateTime NextFPSUpdate = DateTime.Now.AddSeconds(1);
+        void displayFPS()
+        {
+            if (DateTime.Now >= NextFPSUpdate)
+            {
+                this.Text = String.Format("CT Visualizer (fps = {0})", FrameCount);
+                NextFPSUpdate = DateTime.Now.AddSeconds(1);
+                FrameCount = 0;
+            }
+            FrameCount++;
+        }
         public Form1()
         {
             InitializeComponent();
-            b.readBIN("E:\\projects\\panov_tomogram_visualizer\\testdata.bin");
+            bin = new Bin();
+            view = new View();
+            //bin.readBIN("E:\\projects\\panov_tomogram_visualizer\\testdata.bin");
+        }
+
+        private void открытьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog d = new OpenFileDialog();
+            if (d.ShowDialog() == DialogResult.OK)
+            {
+                string s = d.FileName;
+                bin.readBIN(s);
+                view.SetupView(glControl1.Width, glControl1.Height);
+                isLoaded = true;
+                glControl1.Invalidate();
+                trackBar1.Maximum = Bin.Z - 1;
+            }
+        }
+
+        private void glControl1_Paint(object sender, PaintEventArgs e)
+        {
+            if (isLoaded)
+            {
+                view.DrawQuads(curLayer);
+                glControl1.SwapBuffers();
+            }
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            curLayer = trackBar1.Value;
+        }
+
+        void Application_Idle(object sender, EventArgs e)
+        {
+            while (glControl1.IsIdle)
+            {
+                displayFPS();
+                glControl1.Invalidate();
+            }
+        }
+
+       void glControl1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Application.Idle += Application_Idle;
         }
     }
 }
--- panov_tomogram_visualizer/panov_tomogram_visualizer/show/View.cs
+++ panov_tomogram_visualizer/panov_tomogram_visualizer/show/View.cs
@@ -3,14 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
 using OpenTK;
 using OpenTK.Graphics.OpenGL;
 
-namespace panov_tomogram_visualizer.show
+namespace panov_tomogram_visualizer
 {
     class View
     {
-        void SetupView(int width, int height)
+        public void SetupView(int width, int height)
         {
 
               GL.ShadeModel(ShadingModel.Smooth);
@@ -18,7 +19,53 @@ namespace panov_tomogram_visualizer.show
               GL.LoadIdentity();
               GL.Ortho(0, Bin.X, 0, Bin.Y, -1, 1);
               GL.Viewport(0,0,width, height); 
-
         }
+        public void DrawQuads(int layerNum)
+        {
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+            GL.Begin(BeginMode.Quads);
+
+            for (int x = 0; x < Bin.X - 1; x++)
+                for(int y = 0; y < Bin.Y - 1; y++)
+                {
+                    short val;
+                    //1st vertex
+                    val = Bin.ar[x + y * Bin.X + layerNum * Bin.X*Bin.Y];
+                    GL.Color3(TransferFunction(val));
+                    GL.Vertex2(x, y);
+
+                    //2nd vertex
+                    val = Bin.ar[x + (y + 1)* Bin.X + layerNum * Bin.X * Bin.Y];
+                    GL.Color3(TransferFunction(val));
+                    GL.Vertex2(x, y + 1);
+
+                    //3th vertex
+                    val = Bin.ar[x + 1 +(1 + y) * Bin.X + layerNum * Bin.X * Bin.Y];
+                    GL.Color3(TransferFunction(val));
+                    GL.Vertex2(x + 1, y + 1);
+
+                    //4th vertex
+                    val = Bin.ar[x + 1 + y * Bin.X + layerNum * Bin.X * Bin.Y];
+                    GL.Color3(TransferFunction(val));
+                    GL.Vertex2(x + 1, y);
+                }
+
+            GL.End();
+        }
+        public int clamp(int value, int min, int max) // clamp = сцеплять
+        {
+            if (value < min)
+                return min;
+            if (value > max)
+                return max;
+            return value;
+        }
+        public Color TransferFunction(short value)
+        {
+            int min = 0;
+            int max = 2000;
+            int newVal = clamp((value - min) * 255 / (max - min), 0, 255);
+            return Color.FromArgb(255, newVal, newVal, newVal);
+        }     
     }
 }
